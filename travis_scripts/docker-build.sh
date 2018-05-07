59b82b79e8b069785ea295270bba30987cbec714
mikhaelsantos <mikhaelsantos@gmail.com>
2018-05-07 11:39:39 +0300

    Remove travis condition

--- travis_scripts/docker-build.sh
+++ travis_scripts/docker-build.sh
@@ -18,6 +18,4 @@ docker build \
 docker tag ${GIT_TAG} ${BUILD_TAG}
 docker tag ${GIT_TAG} ${LATEST_TAG}
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 docker push ${LATEST_TAG}
-fi
